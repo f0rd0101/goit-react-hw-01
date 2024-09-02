3c3ed41f5e721767ac89908932e7271da274f112
Nick <melancholic8078@gmail.com>
2024-09-02 08:49:27 +0300

    finall

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Profile from './components/Profile/Profile'
 import TransactionHistory from './components/TransactionHistory/TransactionHistory'
 import userData from './assets/userData.json'
-import FriendListItem from './components/FriendList/FriendListItem'
+import FriendListItem from './components/FriendListItem/FriendListItem'
 import friends from './assets/friends.json'
 import transaction from './assets/transaction.json'
 
